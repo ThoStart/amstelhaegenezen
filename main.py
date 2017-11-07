e5d2b7d7b5b7b4026b883ce7c4a92875bd625c2f
Alex Witkamp <alexwitkamp@gmail.com>
2017-11-07 15:32:00 +0100

    __init__ and main python files

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from class_objects import House, Ground, Water
+import matrix as matrix 
+
+house_e = House("Eengezinswoning", 16, 16, 4, 285000, 3)
+house_b = House("Bungalow", 20, 15, 6, 399000, 4)
+house_m = House("Maison", 22, 21, 12, 610000, 6)
+
+ground = Ground()
+
