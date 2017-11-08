cf344e521b4eec2cf689a01e8855efdbbb8b0256
Thomas Start <thomasstart@hotmail.com>
2017-11-08 10:38:07 +0100

    matrix is now filled with v

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-import numpy as np
 from class_objects import House, Ground, Water
-import matrix as matrix 
+import matrix as matrix
 
 house_e = House("Eengezinswoning", 16, 16, 4, 285000, 3)
 house_b = House("Bungalow", 20, 15, 6, 399000, 4)
@@ -10,3 +9,9 @@ house_m = House("Maison", 22, 21, 12, 610000, 6)
 
 ground = Ground()
 
+def main():
+	grid = matrix.init(5,5)
+	print(grid)
+
+if __name__ == "__main__":
+	main()
--- matrix.py
+++ matrix.py
@@ -5,16 +5,23 @@ def init(x, y):
     # put x and y size to half meters
     x = x * 2
     y = y * 2
-    matrix = np.empty([x, y], dtype=int)
+    matrix = np.empty([x, y], dtype=str)
+    matrix.fill('v')
+    # print(matrix)
     return(matrix)
 
+# max_maison = 3, max_bungalow = 5, max_eengezinswoning = 12
 
-# functie: check zijn welk huis past
-def check():
-
-# functie: beste huis pakken
-def choice():
-
-    # functie: huis plaatsen
-def fill(matrix)
-    #matrix.nparray(..., ...) = (...,...)
+# # functie: check zijn welk huis past
+# def check():
+#     # check multiple fields at once
+#
+# # functie: beste huis pakken
+# def choice():
+#
+# # place houses random
+# def random_place():
+#
+#     # functie: huis plaatsen
+# def fill(matrix)
+#     #matrix.nparray(..., ...) = (...,...)
