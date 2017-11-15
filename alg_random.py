c6f1c4b1988c50bc3d700ab038da1ec544b48dc5
Alex Witkamp <alexwitkamp@gmail.com>
2017-11-15 13:51:12 +0100

    added number of houses in setup

--- alg_random.py
+++ alg_random.py
@@ -5,81 +5,80 @@ import house_dictionary as hd
 
 def fill(grid, matrix):
 	# place 7 sets of houses
-	for i in range(3):
 
-		# Eengezinswoningen
+	# Eengezinswoningen
 
-		# random x and y
-		for i in range(info.house_e_ratio):
+	# random x and y
+	for i in range(info.house_e_number):
+
+		# check if house can be placed
+		for i in range (10000):
+			# random x and y
+			x_coordinate = random.randint(info.house_e_free, info.grid_length - info.house_e_length - info.house_e_free)
+			y_coordinate = random.randint(info.house_e_free, info.grid_width - info.house_e_width - info.house_e_free)
+
+			check = matrix.check(info.house_e_length, info.house_e_width, info.house_e_free, x_coordinate, y_coordinate)
+			if check == 0:
+				break
+
+		# place house
+		if check == 0:
+			# create and place house
+			hd.houses_e[hd.house_e_counter] = House(info.house_e_type, hd.house_e_counter, info.house_e_free, info.house_e_value)
+
+			matrix.place(info.house_e_length, info.house_e_width, x_coordinate, y_coordinate, ("E{0:02}".format(hd.houses_e[hd.house_e_counter].id)))
+
+			print("success E{0:02}".format(hd.houses_e[hd.house_e_counter].id))
+
+			hd.house_e_counter = hd.house_e_counter + 1
+
+
+	# Bungalows
+
+	for i in range(info.house_b_number):
+
+		for i in range(10000):
+			# random x and y
+			x_coordinate = random.randint(info.house_b_free, info.grid_length - info.house_b_length - info.house_b_free)
+			y_coordinate = random.randint(info.house_b_free, info.grid_width - info.house_b_width - info.house_b_free)
 
 			# check if house can be placed
-			for i in range (10000):
-				# random x and y
-				x_coordinate = random.randint(info.house_e_free, info.grid_length - info.house_e_length - info.house_e_free)
-				y_coordinate = random.randint(info.house_e_free, info.grid_width - info.house_e_width - info.house_e_free)
-
-				check = matrix.check(info.house_e_length, info.house_e_width, info.house_e_free, x_coordinate, y_coordinate)
-				if check == 0:
-					break
-
-			# place house
+			check = matrix.check(info.house_b_length, info.house_b_width, info.house_b_free, x_coordinate, y_coordinate)
 			if check == 0:
-				# create and place house
-				hd.houses_e[hd.house_e_counter] = House(info.house_e_type, hd.house_e_counter, info.house_e_free, info.house_e_value)
+				break
 
-				matrix.place(info.house_e_length, info.house_e_width, x_coordinate, y_coordinate, ("E{0:02}".format(hd.houses_e[hd.house_e_counter].id)))
+		# place house
+		if check == 0:
+			# create and place house
+			hd.houses_b[hd.house_b_counter] = House(info.house_b_type, hd.house_b_counter, info.house_b_free, info.house_b_value)
 
-				print("success E{0:02}".format(hd.houses_e[hd.house_e_counter].id))
+			matrix.place(info.house_b_length, info.house_b_width, x_coordinate, y_coordinate, ("B{0:02}".format(hd.houses_b[hd.house_b_counter].id)))
 
-				hd.house_e_counter = hd.house_e_counter + 1
+			print("success B{0:02}".format(hd.houses_b[hd.house_b_counter].id))
 
+			hd.house_b_counter = hd.house_b_counter + 1
 
-		# Bungalows
+	# Maisons
 
-		for i in range(info.house_b_ratio):
+	for i in range(info.house_m_number):
 
-			for i in range(10000):
-				# random x and y
-				x_coordinate = random.randint(info.house_b_free, info.grid_length - info.house_b_length - info.house_b_free)
-				y_coordinate = random.randint(info.house_b_free, info.grid_width - info.house_b_width - info.house_b_free)
+		for i in range(10000):
+			# random x and y
+			x_coordinate = random.randint(info.house_m_free, info.grid_length - info.house_m_length - info.house_m_free)
+			y_coordinate = random.randint(info.house_m_free, info.grid_width - info.house_m_width - info.house_m_free)
 
-				# check if house can be placed
-				check = matrix.check(info.house_b_length, info.house_b_width, info.house_b_free, x_coordinate, y_coordinate)
-				if check == 0:
-					break
-
-			# place house
+			# check if house can be placed
+			check = matrix.check(info.house_m_length, info.house_m_width, info.house_m_free, x_coordinate, y_coordinate)
 			if check == 0:
-				# create and place house
-				hd.houses_b[hd.house_b_counter] = House(info.house_b_type, hd.house_b_counter, info.house_b_free, info.house_b_value)
+				break
 
-				matrix.place(info.house_b_length, info.house_b_width, x_coordinate, y_coordinate, ("B{0:02}".format(hd.houses_b[hd.house_b_counter].id)))
+		# place house
+		if check == 0:
+			# create and place house
+			hd.houses_m[hd.house_m_counter] = House(info.house_m_type, hd.house_m_counter, info.house_m_free, info.house_m_value)
 
-				print("success B{0:02}".format(hd.houses_b[hd.house_b_counter].id))
+			matrix.place(info.house_m_length, info.house_m_width, x_coordinate, y_coordinate, ("M{0:02}".format(hd.houses_m[hd.house_m_counter].id)))
 
-				hd.house_b_counter = hd.house_b_counter + 1
+			print("success M{0:02}".format(hd.houses_m[hd.house_m_counter].id))
 
-		# Maisons
-
-		for i in range(info.house_m_ratio):
-
-			for i in range(10000):
-				# random x and y
-				x_coordinate = random.randint(info.house_m_free, info.grid_length - info.house_m_length - info.house_m_free)
-				y_coordinate = random.randint(info.house_m_free, info.grid_width - info.house_m_width - info.house_m_free)
-
-				# check if house can be placed
-				check = matrix.check(info.house_m_length, info.house_m_width, info.house_m_free, x_coordinate, y_coordinate)
-				if check == 0:
-					break
-
-			# place house
-			if check == 0:
-				# create and place house
-				hd.houses_m[hd.house_m_counter] = House(info.house_m_type, hd.house_m_counter, info.house_m_free, info.house_m_value)
-
-				matrix.place(info.house_m_length, info.house_m_width, x_coordinate, y_coordinate, ("M{0:02}".format(hd.houses_m[hd.house_m_counter].id)))
-
-				print("success M{0:02}".format(hd.houses_m[hd.house_m_counter].id))
-
-				hd.house_m_counter = hd.house_m_counter + 1
+			hd.house_m_counter = hd.house_m_counter + 1
--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 # scale is 1:scale
 scale = 2
 
+number_of_houses = 60
+
 # define the grid
 grid_length = 160 * scale
 grid_width = 180 * scale
@@ -13,7 +15,7 @@ house_e_width = 8 * scale
 house_e_free = 2 * scale
 house_e_value = 285000
 house_e_increment = 3
-house_e_ratio = 12
+house_e_number = int(12 * number_of_houses / 20)
 
 # define type, length, width, free space, value, and incrementation for Bungalow
 house_b_type = "Bungalow"
@@ -22,7 +24,7 @@ house_b_width = int(7.5 * scale)
 house_b_free = 3 * scale
 house_b_value = 399000
 house_b_increment = 4
-house_b_ratio = 5
+house_b_number = int(5 * number_of_houses / 20)
 
 # define type, length, width, free space, value, and incrementation for Maison
 house_m_type = "Maison"
@@ -31,7 +33,7 @@ house_m_width = int(10.5 * scale)
 house_m_free = 6 * scale
 house_m_value = 610000
 house_m_increment = 6
-house_m_ratio = 3
+house_m_number = int(3 * number_of_houses / 20)
 
 # define type, length, and width for Water
 water_type = "Water"
