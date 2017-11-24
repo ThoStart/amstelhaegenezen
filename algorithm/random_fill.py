88d47f4b6549fb2e60133f60ba3b531eb6b69e7e
Timo den Hartog <timodenhartog@outlook.com>
2017-11-24 12:02:30 +0100

    random functie roteert huis als het niet past

--- algorithm/random_fill.py
+++ algorithm/random_fill.py
@@ -22,64 +22,129 @@ def fill(grid, matrix):
 			if check == 0:
 				break
 
-		# place house
+		# create and place house
 		if check == 0:
-			# create and place house
 			hd.houses_e[hd.house_e_counter] = House(info.house_e_type, ("E{0:02}".format(hd.house_e_counter)), info.house_e_free, info.house_e_value, x_coordinate, y_coordinate)
-
 			matrix.place(info.house_e_length, info.house_e_width, x_coordinate, y_coordinate, hd.houses_e[hd.house_e_counter].id)
 
 			print("success {}".format(hd.houses_e[hd.house_e_counter].id))
 
 			hd.house_e_counter = hd.house_e_counter + 1
 
+		# if check failed, rotate house and check if house can be placed
+		else:
+			for i in range (10000):
+				# random x and y
+				x_coordinate = random.randint(info.house_e_free, info.grid_length - info.house_e_width - info.house_e_free)
+				y_coordinate = random.randint(info.house_e_free, info.grid_width - info.house_e_length - info.house_e_free)
+
+				check = matrix.check(info.house_e_width, info.house_e_length, info.house_e_free, x_coordinate, y_coordinate)
+				if check == 0:
+					break
+
+			# create and place house
+			if check == 0:
+				hd.houses_e[hd.house_e_counter] = House(info.house_e_type, ("E{0:02}".format(hd.house_e_counter)), info.house_e_free, info.house_e_value, x_coordinate, y_coordinate)
+				matrix.place(info.house_e_length, info.house_e_width, x_coordinate, y_coordinate, hd.houses_e[hd.house_e_counter].id)
+
+				print("success {}".format(hd.houses_e[hd.house_e_counter].id))
+
+				hd.house_e_counter = hd.house_e_counter + 1
+
+			# house placement failed
+			else:
+				return 1
 
 	# Bungalows
 
 	for i in range(info.house_b_number):
 
+		# check if house can be placed
 		for i in range(10000):
 			# random x and y
 			x_coordinate = random.randint(info.house_b_free, info.grid_length - info.house_b_length - info.house_b_free)
 			y_coordinate = random.randint(info.house_b_free, info.grid_width - info.house_b_width - info.house_b_free)
 
-			# check if house can be placed
 			check = matrix.check(info.house_b_length, info.house_b_width, info.house_b_free, x_coordinate, y_coordinate)
 			if check == 0:
 				break
 
-		# place house
+		# create and place house
 		if check == 0:
-			# create and place house
 			hd.houses_b[hd.house_b_counter] = House(info.house_b_type, ("B{0:02}".format(hd.house_b_counter)), info.house_b_free, info.house_b_value, x_coordinate, y_coordinate)
-
 			matrix.place(info.house_b_length, info.house_b_width, x_coordinate, y_coordinate, hd.houses_b[hd.house_b_counter].id)
 
 			print("success {}".format(hd.houses_b[hd.house_b_counter].id))
 
 			hd.house_b_counter = hd.house_b_counter + 1
 
+		# if check failed, rotate house and check if house can be placed
+		else:
+			for i in range (10000):
+				# random x and y
+					x_coordinate = random.randint(info.house_b_free, info.grid_length - info.house_b_width - info.house_b_free)
+					y_coordinate = random.randint(info.house_b_free, info.grid_width - info.house_b_length - info.house_b_free)
+
+					check = matrix.check(info.house_b_width, info.house_b_length, info.house_b_free, x_coordinate, y_coordinate)
+					if check == 0:
+						break
+
+			# create and place house
+			if check == 0:
+				hd.houses_b[hd.house_b_counter] = House(info.house_b_type, ("B{0:02}".format(hd.house_b_counter)), info.house_b_free, info.house_b_value, x_coordinate, y_coordinate)
+				matrix.place(info.house_b_length, info.house_b_width, x_coordinate, y_coordinate, hd.houses_b[hd.house_b_counter].id)
+
+				print("success {}".format(hd.houses_b[hd.house_b_counter].id))
+
+				hd.house_b_counter = hd.house_b_counter + 1
+
+			# house placement failed
+			else:
+				return 1
+
 	# Maisons
 
 	for i in range(info.house_m_number):
 
+		# check if house can be placed
 		for i in range(10000):
 			# random x and y
 			x_coordinate = random.randint(info.house_m_free, info.grid_length - info.house_m_length - info.house_m_free)
 			y_coordinate = random.randint(info.house_m_free, info.grid_width - info.house_m_width - info.house_m_free)
 
-			# check if house can be placed
 			check = matrix.check(info.house_m_length, info.house_m_width, info.house_m_free, x_coordinate, y_coordinate)
 			if check == 0:
 				break
 
-		# place house
+		# create and place house
 		if check == 0:
-			# create and place house
 			hd.houses_m[hd.house_m_counter] = House(info.house_m_type, ("M{0:02}".format(hd.house_m_counter)), info.house_m_free, info.house_m_value,  x_coordinate, y_coordinate)
-
 			matrix.place(info.house_m_length, info.house_m_width, x_coordinate, y_coordinate, hd.houses_m[hd.house_m_counter].id)
 
 			print("success {}".format(hd.houses_m[hd.house_m_counter].id))
 
 			hd.house_m_counter = hd.house_m_counter + 1
+
+		# if check failed, rotate house and check if house can be placed
+		else:
+			for i in range(10000):
+				# random x and y
+				x_coordinate = random.randint(info.house_m_free, info.grid_length - info.house_m_width - info.house_m_free)
+				y_coordinate = random.randint(info.house_m_free, info.grid_width - info.house_m_length - info.house_m_free)
+
+				check = matrix.check(info.house_m_width, info.house_m_length, info.house_m_free, x_coordinate, y_coordinate)
+				if check == 0:
+					break
+
+			# create and place house
+			if check == 0:
+				hd.houses_m[hd.house_m_counter] = House(info.house_m_type, ("M{0:02}".format(hd.house_m_counter)), info.house_m_free, info.house_m_value,  x_coordinate, y_coordinate)
+				matrix.place(info.house_m_width, info.house_m_length, x_coordinate, y_coordinate, hd.houses_m[hd.house_m_counter].id)
+
+				print("success {}".format(hd.houses_m[hd.house_m_counter].id))
+
+				hd.house_m_counter = hd.house_m_counter + 1
+
+			# house placement failed
+			else:
+				return 1
