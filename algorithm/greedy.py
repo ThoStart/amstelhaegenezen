220c886697350f5a7c62fbd8be2f5c4e7168bc3e
Thomas Start <thomasstart@hotmail.com>
2017-12-03 12:00:54 +0100

    greedy zou zo bijna moeten werken even samen testen maandag

--- algorithm/greedy.py
+++ algorithm/greedy.py
@@ -33,6 +33,7 @@ def greedy():
     # remember where most free space around the house, put the house there.
 
     # make new house
+    hd.counter_e_counter = 1
     hd.houses_e[hd.house_e_counter] = House(info.house_e_type, ("E{0:02}".format(hd.house_e_counter)), info.house_e_free, info.house_e_value, x_coordinate, y_coordinate, info.house_e_length, info.house_e_width)
     # add it to the map
     matrix.place(info.house_e_length, info.house_e_width, x_coordinate, y_coordinate, hd.houses_e[hd.house_e_counter].id)
@@ -42,19 +43,33 @@ def greedy():
     best_y_cor = 0
     best_free_space = 0
 
-    while free_space_actual < best_free_space:
-        x_coordinate=+1
-        y_coordinate=+1
-        matrix.place(info.house_e_length, info.house_e_width, x_coordinate, y_coordinate, hd.houses_e[hd.house_e_counter].id)
-        free_space_actual = matrix.free_space(grid, hd.houses_m[i], hd.houses_m[i].id, info.house_m_length, info.house_m_width)
+    for x in range(info.grid_width):
+        for y in range(info.grid_length):
+            while hd.house_e_counter in range(# ... SOME CODE MISSING ...):
+                x_coordinate=+1
+                y_coordinate=+1
 
-                if (x >= info.grid_length or y >= info.grid_width):
-                    free_space_actual = free_space_actual - 1
-                    return free_space_actual - 1
+                # if house would have been placed onto another house move it
+                if self.grid[x_coordinate, y_coordinate] != 'v':
+                    x_coordinate=+1
+                    y_coordinate=+1
 
-                elif (self.grid[x, y] != 'v' and self.grid[x, y] != name):
-                    free_space_actual = free_space_actual - 1
-                    return free_space_actual - 1
+                if (x_coordinate >= info.grid_length or y_coordinate >= info.grid_width):
+                        matrix.place(info.house_e_length, info.house_e_width, best_x_cor, best_y_cor, hd.houses_e[hd.house_e_counter].id)
+                        hd.house_e_counter+=1
+                        print("success {}".format(hd.houses_e[hd.house_e_counter].id))
 
 
-    print("success {}".format(hd.houses_e[hd.house_e_counter].id))
+                        return # ... SOME CODE MISSING ...
+
+                # place house in top-left corner
+                matrix.place(info.house_e_length, info.house_e_width, x_coordinate, y_coordinate, hd.houses_e[hd.house_e_counter].id)
+
+                # calculate free space of just placed house
+                free_space_actual = matrix.free_space(grid, hd.houses_m[i], hd.houses_m[i].id, info.house_m_length, info.house_m_width)
+
+                    # replace house if actual free_space is higher than current best free space coordinates
+                    elif (free_space_actual > best_free_space)
+                        best_x_cor = x_coordinate
+                        best_y_cor = y_coordinate
+                        best_free_space = matrix.free_space(grid, hd.houses_m[i], hd.houses_m[i].id, info.house_m_length, info.house_m_width)
