e00b816d3d52489ef98142bbde33c5f3d2b1a0f9
Alex Witkamp <alexwitkamp@gmail.com>
2017-12-12 17:07:42 +0100

    HC now has random swap 1000x

--- algorithm/hill_climbing.py
+++ algorithm/hill_climbing.py
@@ -2,33 +2,93 @@ from classes.class_objects import House, Water, Matrix
 import library.setup as info
 import library.house_dictionary as hd
 import library.score as score
+import random
 
 def start(matrix, grid):
-    hc_data = []
+	hc_data = []
 
-    execute_swap(grid, matrix, hc_data, hd.houses_e, hd.houses_b)
-    execute_swap(grid, matrix, hc_data, hd.houses_b, hd.houses_m)
-    execute_swap(grid, matrix, hc_data, hd.houses_m, hd.houses_e)
-    execute_swap(grid, matrix, hc_data, hd.houses_e, hd.houses_b)
+	execute_swap(grid, matrix, hc_data, hd.houses_e, hd.houses_b)
+	execute_swap(grid, matrix, hc_data, hd.houses_b, hd.houses_m)
+	execute_swap(grid, matrix, hc_data, hd.houses_m, hd.houses_e)
+	execute_swap(grid, matrix, hc_data, hd.houses_e, hd.houses_b)
 
-    print(hc_data)
+	number_of_iterations = 1000 - len(hc_data) - len(hc_data)
 
-    number_of_iterations = 1000 - len(hc_data)
+	for times in range(int(number_of_iterations / info.number_of_houses)):
+		for i in hd.houses_e:
+			if len(hc_data) >= 1000:
+				break
+			execute_random(grid, matrix, hc_data,hd.houses_e[i])
+		for i in hd.houses_b:
+			if len(hc_data) >= 1000:
+				break
+			execute_random(grid, matrix, hc_data,hd.houses_b[i])
+		for i in hd.houses_m:
+			if len(hc_data) >= 1000:
+				break
+			execute_random(grid, matrix, hc_data,hd.houses_m[i])
 
-    return hc_data
+	execute_swap(grid, matrix, hc_data, hd.houses_e, hd.houses_b)
+	execute_swap(grid, matrix, hc_data, hd.houses_b, hd.houses_m)
+	execute_swap(grid, matrix, hc_data, hd.houses_m, hd.houses_e)
+	execute_swap(grid, matrix, hc_data, hd.houses_e, hd.houses_b)
+
+	print(len(hc_data))
+
+	print(hc_data)
+
+	return hc_data
 
 def execute_swap(grid, matrix, hc_data, houses_a, houses_b):
-    total_score = score.calculate(grid, matrix)
+	total_score = score.calculate(grid, matrix)
 
-    for i in houses_a:
-        for j in houses_b:
-            Matrix.swap(matrix, grid, houses_a[i], houses_b[j])
-            total_score_new = score.calculate(grid, matrix)
+	for i in houses_a:
+		for j in houses_b:
+			Matrix.swap(matrix, grid, houses_a[i], houses_b[j])
+			total_score_new = score.calculate(grid, matrix)
 
-            if total_score_new < total_score:
-                hc_data.append(total_score)
-                Matrix.swap(matrix, grid, houses_a[i], houses_b[j])
-                total_score = score.calculate(grid, matrix)
-            else:
-                total_score = total_score_new
-                hc_data.append(total_score)
+			if total_score_new < total_score:
+				hc_data.append(total_score)
+				Matrix.swap(matrix, grid, houses_a[i], houses_b[j])
+				total_score = score.calculate(grid, matrix)
+			else:
+				total_score = total_score_new
+				hc_data.append(total_score)
+
+def execute_random(grid, matrix, hc_data, house):
+	total_score = score.calculate(grid, matrix)
+
+	house_x_free = eval("info.house_" + str.lower(house.id[:1]) + "_free")
+	house_x_length = eval("info.house_" + str.lower(house.id[:1]) + "_length")
+	house_x_width = eval("info.house_" + str.lower(house.id[:1]) + "_width")
+	house_x_value = eval("info.house_" + str.lower(house.id[:1]) + "_value")
+	house_x_type = eval("info.house_" + str.lower(house.id[:1]) + "_type")
+
+	# check if house can be placed
+	for i in range (10000):
+		# random x and y
+		x_coordinate = random.randint(house_x_free, info.grid_length - house_x_length - house_x_free)
+		y_coordinate = random.randint(house_x_free, info.grid_width - house_x_width - house_x_free)
+
+		check = matrix.check(house_x_length, house_x_width, house_x_free, x_coordinate, y_coordinate)
+		if check == 0:
+			break
+
+
+	# create and place house
+	if check == 0:
+		temp_house = House(house_x_type, 'v', house_x_free, house_x_value, x_coordinate, y_coordinate, house_x_length, house_x_width)
+
+		Matrix.swap(matrix, grid, house, temp_house)
+		total_score_new = score.calculate(grid, matrix)
+
+		if total_score_new < total_score:
+			hc_data.append(total_score)
+			Matrix.swap(matrix, grid, house, temp_house)
+			total_score = score.calculate(grid, matrix)
+		else:
+			total_score = total_score_new
+			hc_data.append(total_score)
+	else:
+		total_score = total_score_new
+		hc_data.append(total_score)
--- classes/class_objects.py
+++ classes/class_objects.py
@@ -2,134 +2,138 @@ import numpy as np
 import library.setup as info
 
 class House:
-    def __init__(self, type, id, free, value, xcor, ycor, length, width):
-        self.type = type
-        self.id = id
-        self.free = free
-        self.value = value
-        self.xcor = xcor
-        self.ycor = ycor
-        self.length = length
-        self.width = width
+	def __init__(self, type, id, free, value, xcor, ycor, length, width):
+		self.type = type
+		self.id = id
+		self.free = free
+		self.value = value
+		self.xcor = xcor
+		self.ycor = ycor
+		self.length = length
+		self.width = width
 
 class Water:
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
+	def __init__(self, length, width):
+		self.length = length
+		self.width = width
 
 class Matrix:
-    def __init__(self, xsize, ysize):
-        self.xsize = xsize
-        self.ysize = ysize
-        self.grid = np.full((self.xsize, self.ysize), fill_value='xxx', dtype=None)
-        self.grid.fill('v')
-        #return(grid)
+	def __init__(self, xsize, ysize):
+		self.xsize = xsize
+		self.ysize = ysize
+		self.grid = np.full((self.xsize, self.ysize), fill_value='xxx', dtype=None)
+		self.grid.fill('v')
+		#return(grid)
 
-    # finds empty coordinate
-    def findemptyspot(self, xsize, ysize):
-        for x in range(xsize):
-            for y in range(ysize):
-                if (self.grid[x, y] == 'v'):
-                    return x,y
-        else:
-            return 0
+	# finds empty coordinate
+	def findemptyspot(self, xsize, ysize):
+		for x in range(xsize):
+			for y in range(ysize):
+				if (self.grid[x, y] == 'v'):
+					return x,y
+		else:
+			return 0
 
-    # random kiezen welk huis meest waard is
-    # def worth
-    # dus eerste Eengezinswoning die is namelijk het meeste waard
+	# random kiezen welk huis meest waard is
+	# def worth
+	# dus eerste Eengezinswoning die is namelijk het meeste waard
 
 
-    # checks if enough space around empty coordinate for house
-    def check(self, x_opp, y_opp, free, x_coordinate, y_coordinate):
-        # search for empty spaces
-        for x in range((x_coordinate - free), (x_opp + x_coordinate + free)):
-            for y in range((y_coordinate - free), (y_opp + y_coordinate + free)):
-                if (x >= info.grid_length or y >= info.grid_width or x < 0 or y < 0 or self.grid[x, y] != 'v'):
-                    return 1
+	# checks if enough space around empty coordinate for house
+	def check(self, x_opp, y_opp, free, x_coordinate, y_coordinate):
+		# search for empty spaces
+		for x in range((x_coordinate - free), (x_opp + x_coordinate + free)):
+			for y in range((y_coordinate - free), (y_opp + y_coordinate + free)):
+				if (x >= info.grid_length or y >= info.grid_width or x < 0 or y < 0 or self.grid[x, y] != 'v'):
+					return 1
 
-        # check if house isn't interfering with Bungalow
-        for x in range((x_coordinate - info.house_b_free), (x_opp + x_coordinate + info.house_b_free)):
-            for y in range((y_coordinate - info.house_b_free), (y_opp + y_coordinate + info.house_b_free)):
-                if (x >= info.grid_length or y >= info.grid_width or x < 0 or y < 0 or 'B' in self.grid[x, y]):
-                    return 1
+		# check if house isn't interfering with Bungalow
+		for x in range((x_coordinate - info.house_b_free), (x_opp + x_coordinate + info.house_b_free)):
+			for y in range((y_coordinate - info.house_b_free), (y_opp + y_coordinate + info.house_b_free)):
+				if (x >= info.grid_length or y >= info.grid_width or x < 0 or y < 0 or 'B' in self.grid[x, y]):
+					return 1
 
-        # check if house isn't interfering with Maison
-        for x in range((x_coordinate - info.house_m_free), (x_opp + x_coordinate + info.house_m_free)):
-            for y in range((y_coordinate - info.house_m_free), (y_opp + y_coordinate + info.house_m_free)):
-                if (x >= info.grid_length or y >= info.grid_width or x < 0 or y < 0 or 'M' in self.grid[x, y]):
-                    return 1
-        return 0
-    # a = np.where(matrix == 'v')
-    # print(a)
+		# check if house isn't interfering with Maison
+		for x in range((x_coordinate - info.house_m_free), (x_opp + x_coordinate + info.house_m_free)):
+			for y in range((y_coordinate - info.house_m_free), (y_opp + y_coordinate + info.house_m_free)):
+				if (x >= info.grid_length or y >= info.grid_width or x < 0 or y < 0 or 'M' in self.grid[x, y]):
+					return 1
+		return 0
+	# a = np.where(matrix == 'v')
+	# print(a)
 
-    # puts a house on the empty space
-    def place(self, x_opp, y_opp, x_coordinate, y_coordinate, name):
-        for x in range(x_coordinate, x_opp+x_coordinate):
-            for y in range(y_coordinate, y_opp+y_coordinate):
-                self.grid[x, y] = name
+	# puts a house on the empty space
+	def place(self, x_opp, y_opp, x_coordinate, y_coordinate, name):
+		for x in range(x_coordinate, x_opp+x_coordinate):
+			for y in range(y_coordinate, y_opp+y_coordinate):
+				self.grid[x, y] = name
 
-        #return(grid)
+		#return(grid)
 
-    def export(self, grid):
-        np.savetxt('grid.csv', grid, fmt='%s', delimiter=',')
+	def export(self, grid):
+		np.savetxt('grid.csv', grid, fmt='%s', delimiter=',')
 
-    def free_space(self, grid, house, name, x_opp, y_opp):
-        free = 1
+	def free_space(self, grid, house, name, x_opp, y_opp):
+		free = 1
 
-        while(free > 0):
-            #for x in set.union(set(range(house.xcor - free, house.xcor - free + 1)), set(range(house.xcor + x_opp + free - 1, house.xcor + x_opp + free))):
-            for x in set.union(set(range(house.xcor - free, house.xcor)), set(range(house.xcor + x_opp, house.xcor + x_opp + free))):
-            #for x in range((house.xcor - free), x_opp + free+ house.xcor):
-                #for y in set.union(set(range(house.ycor - free, house.ycor - free + 1)), set(range(house.ycor + y_opp + free - 1, house.ycor + y_opp + free))):
-                for y in set.union(set(range(house.ycor - free, house.ycor)), set(range(house.ycor + y_opp, house.ycor + y_opp + free))):
-                #for y in range((house.ycor - free), y_opp + free + house.ycor):
-                    if (x >= info.grid_length or y >= info.grid_width or x < 0 or y < 0):
-                        house.free = free - 1
-                        print(house.free)
-                        return free - 1
-                    elif (self.grid[x, y] != 'v' and self.grid[x, y] != name):
-                        house.free = free - 1
-                        print(house.free)
-                        return free - 1
+		while(free > 0):
+			#for x in set.union(set(range(house.xcor - free, house.xcor - free + 1)), set(range(house.xcor + x_opp + free - 1, house.xcor + x_opp + free))):
+			for x in set.union(set(range(house.xcor - free, house.xcor)), set(range(house.xcor + x_opp, house.xcor + x_opp + free))):
+			#for x in range((house.xcor - free), x_opp + free+ house.xcor):
+				#for y in set.union(set(range(house.ycor - free, house.ycor - free + 1)), set(range(house.ycor + y_opp + free - 1, house.ycor + y_opp + free))):
+				for y in set.union(set(range(house.ycor - free, house.ycor)), set(range(house.ycor + y_opp, house.ycor + y_opp + free))):
+				#for y in range((house.ycor - free), y_opp + free + house.ycor):
+					if (x >= info.grid_length or y >= info.grid_width or x < 0 or y < 0):
+						house.free = free - 1
+						print(house.free)
+						return free - 1
+					elif (self.grid[x, y] != 'v' and self.grid[x, y] != name):
+						house.free = free - 1
+						print(house.free)
+						return free - 1
 
-            free+=1
+			free+=1
 
-        return
+		return
 
-    def score (self, house, scale, default_value, default_free, default_increment):
-        free = (int((house.free - default_free) / scale) * default_increment)
+	def score (self, house, scale, default_value, default_free, default_increment):
+		free = (int((house.free - default_free) / scale) * default_increment)
 
-        house.value = int(default_value * (free + 1))
+		house.value = int(default_value * (free + 1))
 
-        return house.value
+		return house.value
 
-    def swap(self, grid, house_a, house_b):
-    	# clear houses
-    	self.place(house_a.length, house_a.width, house_a.xcor, house_a.ycor, 'v')
-    	self.place(house_b.length, house_b.width, house_b.xcor, house_b.ycor, 'v')
+	def swap(self, grid, house_a, house_b):
+		# clear houses
+		self.place(house_a.length, house_a.width, house_a.xcor, house_a.ycor, 'v')
+		self.place(house_b.length, house_b.width, house_b.xcor, house_b.ycor, 'v')
 
-    	house_a_xcor_new = int(house_b.xcor + int(1/2 * house_b.length) - int(1/2 * house_a.length))
-    	house_a_ycor_new = int(house_b.ycor + int(1/2 * house_b.width) - int(1/2 * house_a.width))
-    	house_b_xcor_new = int(house_a.xcor + int(1/2 * house_a.length) - int(1/2 * house_b.length))
-    	house_b_ycor_new = int(house_a.ycor + int(1/2 * house_a.width) - int(1/2 * house_b.width))
+		house_a_xcor_new = int(house_b.xcor + int(1/2 * house_b.length) - int(1/2 * house_a.length))
+		house_a_ycor_new = int(house_b.ycor + int(1/2 * house_b.width) - int(1/2 * house_a.width))
+		house_b_xcor_new = int(house_a.xcor + int(1/2 * house_a.length) - int(1/2 * house_b.length))
+		house_b_ycor_new = int(house_a.ycor + int(1/2 * house_a.width) - int(1/2 * house_b.width))
 
-    	# check if houses can be swapped
-    	check_a = self.check(house_a.length, house_a.width, eval("info.house_" + str.lower(house_a.id[:1]) + "_free"), house_a_xcor_new, house_a_ycor_new)
-    	check_b = self.check(house_b.length, house_b.width, eval("info.house_" + str.lower(house_b.id[:1]) + "_free"), house_b_xcor_new, house_b_ycor_new)
-    	print("checks: {}, {}" .format(check_a, check_b))
+		# check if houses can be swapped
+		check_a = self.check(house_a.length, house_a.width, eval("info.house_" + str.lower(house_a.id[:1]) + "_free"), house_a_xcor_new, house_a_ycor_new)
 
-    	# if houses can be swapped, place houses on new location
-    	if (check_a == 0 and check_b == 0):
-    		print("old: house_a.xcor, ycor: {}, {}" .format(house_a.xcor, house_a.ycor))
-    		self.place(house_a.length, house_a.width, house_a_xcor_new, house_a_ycor_new, house_a.id)
-    		self.place(house_b.length, house_b.width, house_b_xcor_new, house_b_ycor_new, house_b.id)
+		if house_b.id == 'v':
+			check_b = self.check(house_b.length, house_b.width, eval("info.house_" + str.lower(house_a.id[:1]) + "_free"), house_b_xcor_new, house_b_ycor_new)
+		else:
+			check_b = self.check(house_b.length, house_b.width, eval("info.house_" + str.lower(house_b.id[:1]) + "_free"), house_b_xcor_new, house_b_ycor_new)
+		print("checks: {}, {}" .format(check_a, check_b))
 
-    		# update dictionary
-    		house_a.xcor, house_a.ycor = house_a_xcor_new, house_a_ycor_new
-    		house_b.xcor, house_b.ycor = house_b_xcor_new, house_b_ycor_new
-    		print("new: house_a.xcor, ycor: {}, {}" .format(house_a.xcor, house_a.ycor))
+		# if houses can be swapped, place houses on new location
+		if (check_a == 0 and check_b == 0):
+			print("old: house_a.xcor, ycor: {}, {}" .format(house_a.xcor, house_a.ycor))
+			self.place(house_a.length, house_a.width, house_a_xcor_new, house_a_ycor_new, house_a.id)
+			self.place(house_b.length, house_b.width, house_b_xcor_new, house_b_ycor_new, house_b.id)
 
-    	# if houses cannot be swapped, place houses on original location
-    	else:
-    		self.place(house_a.length, house_a.width, house_a.xcor, house_a.ycor, house_a.id)
-    		self.place(house_b.length, house_b.width, house_b.xcor, house_b.ycor, house_b.id)
+			# update dictionary
+			house_a.xcor, house_a.ycor = house_a_xcor_new, house_a_ycor_new
+			house_b.xcor, house_b.ycor = house_b_xcor_new, house_b_ycor_new
+			print("new: house_a.xcor, ycor: {}, {}" .format(house_a.xcor, house_a.ycor))
+
+		# if houses cannot be swapped, place houses on original location
+		else:
+			self.place(house_a.length, house_a.width, house_a.xcor, house_a.ycor, house_a.id)
+			self.place(house_b.length, house_b.width, house_b.xcor, house_b.ycor, house_b.id)
